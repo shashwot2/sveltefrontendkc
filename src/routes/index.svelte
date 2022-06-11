0dd75bf7711e00c2cbca4b63d64ecf1490a01be5
shashwot2 <shashwot_07@hotmail.com>
2022-06-12 00:34:38 +0700

    Added centered nav-bar and test footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div class="navigation">
-    <ul id="nav-bar">>
+    <ul>
         <li><a class="active" href="#Home">Home</a></li>
         <li><a href="#about">About</a></li>
        <li><a href="#projects">Projects</a></li>
@@ -10,6 +10,10 @@
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href='https://www.freepik.com/vectors/software-background'>Software background</a>
 <button on:click|once={handleClick}>Click me</button>
+
+<div class ="footer">
+    <p> Footer </p>
+</div>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap');
     :global(body) {
@@ -24,26 +28,36 @@
         color: white;
         text-align: center;
     }
+    .navigation{
+        height: 3.5em;
+        text-align: center;
+    }
     .navigation ul {
-        list-style-type: none;
-        margin: 0;
-        padding: 0;
+        display: inline-block;
+        list-style-type: none;  
         overflow: hidden;
         background-color: transparent;
     }
 
     .navigation li {
         color: white;
-        float:center;
+        float: center;
+        display :inline;
     }
 
     .navigation ul li a {
+        padding: 0 30px;
+        float: left;
         display: block;
         color: white;
-        text-align: center;
-        vertical-align: middle;
-        padding: 14px;
         text-decoration: none;
+    }
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0; 
+        width: 100%;
+        text-align: center;
     }
 </style>
 
