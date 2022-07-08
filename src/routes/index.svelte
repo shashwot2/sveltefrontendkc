38d5baa8163d8a3e6745d76a034bb7d123f8dafe
Shashwot KC <shashwot_07@hotmail.com>
2022-07-08 16:55:26 +0700

    add footer bars

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,38 +1,51 @@
-<div class="navigation">
+<nav class="navigation">
     <ul>
         <li><a class="active" href="#Home">Home</a></li>
         <li><a href="#about">About</a></li>
        <li><a href="#projects">Projects</a></li>
     </ul>
-</div>
-
-<h1>Welcome to SvelteKit</h1>
+</nav>
+<div class = "paragraph">
 <p>
-    Hi! Welcome to my personal website. I'm Shashwot KC. You can call me KC if you like :). This website shows my personal projects and what I get up to in a regular basis. It is also me learning
+
+    Hi! Welcome to my personal website. I'm Shashwot KC. This website shows my personal projects and what I get up to in a regular basis. It is also me learning
     a frontend framework to become a full stack engineer. I'm currently a backend engineer with some server skills to boot. This website was made with svelte.
     
     Blog under construction.
 </p>
-<a href='https://www.freepik.com/vectors/software-background'>Software background</a>
-<button on:click|once={handleClick}>Click me</button>
+</div>
+<img src = "/Dexter.jpg" alt = "Dexter" class = "dexter">
 
 <div class ="footer">
+    <a href="https://github.com/shashwot2"><img class= "footer-links" src = "/git.png" alt = "github"></a>
+    <a href="https://www.linkedin.com/in/shashwot-kc-b9a8b917b/"><img class= "footer-links" src = "/linkedin.png" alt = "linkedin"></a>
     <p> Footer </p>
 </div>
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap');
     :global(body) {
-        background-image: url('/Background.jpg');
         background-size: cover;
+        /* background-image: url('/Dexter.jpg'); */
         font-family: 'Comfortaa', cursive;
+        background-color: #e2e2e2;
     }
-    p {
-        color:white;
+    .dexter{
+        width: 100%;
+        height: 50%;
+        display: block;
         align-items: center;
+        opacity: 0.5;
     }
-    h1 {
-        color: white;
+    .paragraph{
+        display: flex;
+        align-items: center;
         text-align: center;
+        width: 65%;
+        font-size: large;
+        height: 200px;
+        line-height: 1.2;
+        margin: auto;
+        
     }
     .navigation{
         height: 3.5em;
@@ -46,7 +59,7 @@
     }
 
     .navigation li {
-        color: white;
+        color: black;
         float: center;
         display :inline;
     }
@@ -55,20 +68,18 @@
         padding: 0 30px;
         float: left;
         display: block;
-        color: white;
+        color: black;
+        font-size: large;
         text-decoration: none;
     }
     .footer {
-        position: fixed;
+        /* position: fixed; */
         left: 0;
         bottom: 0; 
         width: 100%;
         text-align: center;
     }
-</style>
-
-<script>
-    function handleClick() {
-        alert('stop clicking');
+    .footer .footer-links{
+        width: 40px;
     }
-</script>
+</style>
