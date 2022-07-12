ef764fd2a7c991889f91cd1bb5ee5e2c97ce3374
Shashwot KC <shashwot_07@hotmail.com>
2022-07-12 17:31:12 +0700

    add TailwindCSSw

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,10 @@
-<nav class="navigation">
-    <ul>
-        <li><a class="active" href="#Home">Home</a></li>
+<nav class="h-3.5 text-center ">
+    <ul class ="inline-block bg-transparent overflow-hidden">
+        <div class = "block px-30 float-center text-lg">
+        <li><a href="#Home">Home</a></li>
         <li><a href="#about">About</a></li>
        <li><a href="#projects">Projects</a></li>
+    </div>
     </ul>
 </nav>
 <div class = "paragraph">
@@ -21,65 +23,8 @@
     <a href="https://www.linkedin.com/in/shashwot-kc-b9a8b917b/"><img class= "footer-links" src = "/linkedin.png" alt = "linkedin"></a>
     <p> Footer </p>
 </div>
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap');
-    :global(body) {
-        background-size: cover;
-        /* background-image: url('/Dexter.jpg'); */
-        font-family: 'Comfortaa', cursive;
-        background-color: #e2e2e2;
-    }
-    .dexter{
-        width: 100%;
-        height: 50%;
-        display: block;
-        align-items: center;
-        opacity: 0.5;
-    }
-    .paragraph{
-        display: flex;
-        align-items: center;
-        text-align: center;
-        width: 65%;
-        font-size: large;
-        height: 200px;
-        line-height: 1.2;
-        margin: auto;
-        
-    }
-    .navigation{
-        height: 3.5em;
-        text-align: center;
-    }
-    .navigation ul {
-        display: inline-block;
-        list-style-type: none;  
-        overflow: hidden;
-        background-color: transparent;
-    }
 
-    .navigation li {
-        color: black;
-        float: center;
-        display :inline;
-    }
+<script>
+    import "../app.css";
+</script>
 
-    .navigation ul li a {
-        padding: 0 30px;
-        float: left;
-        display: block;
-        color: black;
-        font-size: large;
-        text-decoration: none;
-    }
-    .footer {
-        /* position: fixed; */
-        left: 0;
-        bottom: 0; 
-        width: 100%;
-        text-align: center;
-    }
-    .footer .footer-links{
-        width: 40px;
-    }
-</style>
