a3c94fe91df0e9103ced7f193bc155f1a4ca97c4
Shashwot KC <shashwot_07@hotmail.com>
2022-08-30 17:11:58 +0700

    Update: fix nav bar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <nav class="h-3.5 text-center ">
-    <ul class ="inline-block bg-transparent overflow-hidden">
-        <div class = "block px-30 float-center text-lg">
+    <ul class ="inline-block items-end bg-transparent overflow-hidden">
+        <div class = "flex px-30 float-center space-x-20 text-lg">
         <li><a href="#Home">Home</a></li>
         <li><a href="#about">About</a></li>
        <li><a href="#projects">Projects</a></li>
@@ -10,10 +10,7 @@
 <div class = "paragraph">
 <p>
 
-    Hi! Welcome to my personal website. I'm Shashwot KC. This website shows my personal projects and what I get up to in a regular basis. It is also me learning
-    a frontend framework to become a full stack engineer. I'm currently a backend engineer with some server skills to boot. This website was made with svelte.
-    
-    Blog under construction.
+    Hi
 </p>
 </div>
 <img src = "/Dexter.jpg" alt = "Dexter" class = "dexter">
